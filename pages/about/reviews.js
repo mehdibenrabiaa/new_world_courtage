3b21c9e3d56a708b733e86f234d4db7d59fbefe4
stephens <todds496@gmail.com>
2026-06-12 11:48:19 -0500

    trustpilot block: scope green border to content width only

--- pages/about/reviews.js
+++ pages/about/reviews.js
@@ -259,9 +259,9 @@ function TrustPilotBlock() {
   const starFills = [1, 1, 1, 1, 0.8];
 
   return (
-    <section className="w-full border-t border-gray-200 border-b-2 border-b-[#00b67a] py-8">
+    <section className="w-full border-t border-gray-200 py-8">
       <div className="px-4 lg:px-12 2xl:px-24 flex justify-center">
-        <div className="inline-flex flex-col items-start gap-2">
+        <div className="inline-flex flex-col items-start gap-2 border-b-2 border-b-[#00b67a] pb-1">
 
           {/* Trustpilot wordmark */}
           <img
