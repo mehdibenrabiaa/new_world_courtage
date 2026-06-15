9d4e71654b5084438ffd731d73735ac6b5d582b8
stephens <todds496@gmail.com>
2026-06-15 03:46:43 -0500

    review content: remove horizontal padding, rely on container margin

--- pages/about/reviews.js
+++ pages/about/reviews.js
@@ -85,7 +85,7 @@ function FeaturedReview() {
 
       {/* Review content */}
       <section className="w-full py-10 lg:py-14">
-        <Container className="flex flex-col gap-6 max-w-3xl mx-auto">
+        <Container className="flex flex-col gap-6 max-w-3xl mx-auto !px-0">
           <div className="flex flex-col gap-1">
             <p className="text-[17px] font-semibold text-[#131212]">Catherine D.</p>
             <p className="text-[13px] text-gray-400">Cliente depuis 2024 &middot; Lyon, Auvergne-Rhône-Alpes</p>
