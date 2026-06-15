a3828fda4f8be33d925b6e01206ec5668473ae93
stephens <todds496@gmail.com>
2026-06-15 03:53:31 -0500

    trustpilot: remove border from text container

--- pages/about/reviews.js
+++ pages/about/reviews.js
@@ -278,7 +278,7 @@ function TrustPilotBlock() {
                 <TpStarBox key={i} fill={fill} />
               ))}
             </div>
-            <div className="flex flex-col gap-0 border border-gray-200 pl-0 pr-4 py-2">
+            <div className="flex flex-col gap-0 pl-0 pr-4 py-2">
               <p className="text-[16px] font-bold text-[#131212]">
                 TrustScore {score} sur 5
               </p>
