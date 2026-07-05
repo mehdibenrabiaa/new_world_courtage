05f5d5139d24db8010fac972ee617313a1bad985
Mehdi <mehdi@gmail.com>
2026-07-05 16:53:24 +0100

    fix: mobile nav drawer slide-in/out animation

--- components/ui/sheet.jsx
+++ components/ui/sheet.jsx
@@ -9,10 +9,7 @@ const SheetPortal = Dialog.Portal;
 function SheetOverlay({ className, ...props }) {
   return (
     <Dialog.Overlay
-      className={cn(
-        "fixed inset-0 z-50 bg-black/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-        className
-      )}
+      className={cn("fixed inset-0 z-50 bg-black/50 sheet-overlay", className)}
       {...props}
     />
   );
@@ -20,17 +17,17 @@ function SheetOverlay({ className, ...props }) {
 
 function SheetContent({ className, children, side = "left", ...props }) {
   const sides = {
-    left:  "inset-y-0 left-0 h-full w-[min(480px,100vw)] data-[state=open]:slide-in-from-left data-[state=closed]:slide-out-to-left",
-    right: "inset-y-0 right-0 h-full w-[min(480px,100vw)] data-[state=open]:slide-in-from-right data-[state=closed]:slide-out-to-right",
-    top:   "inset-x-0 top-0 data-[state=open]:slide-in-from-top data-[state=closed]:slide-out-to-top",
-    bottom:"inset-x-0 bottom-0 data-[state=open]:slide-in-from-bottom data-[state=closed]:slide-out-to-bottom",
+    left:   "inset-y-0 left-0 h-full w-[min(480px,100vw)] sheet-slide-left",
+    right:  "inset-y-0 right-0 h-full w-[min(480px,100vw)]",
+    top:    "inset-x-0 top-0",
+    bottom: "inset-x-0 bottom-0",
   };
   return (
     <SheetPortal>
       <SheetOverlay />
       <Dialog.Content
         className={cn(
-          "fixed z-50 bg-white shadow-2xl flex flex-col overflow-hidden transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-300",
+          "fixed z-50 bg-white shadow-2xl flex flex-col overflow-hidden",
           sides[side],
           className
         )}
